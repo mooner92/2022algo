74bf7db12416497bbf4ecee96afd94bbedc74060
Mooner92 <chlaudgjs0989@gmail.com>
2022-02-23 00:41:54 +0900

    1152

--- 1152-단어의개수.cpp
+++ 1152-단어의개수.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+int main(){
+    
+    string ex1;
+    getline(cin,ex1);
+    int spacecount = 0;
+    if(ex1[0] == ' ')
+        spacecount--;
+
+    if(ex1[ex1.length()-1] == ' ')
+        spacecount--; 
+
+    for(int i=0;i<ex1.length();i++){
+
+        if (ex1[i] == ' ')
+        spacecount++;
+
+        cout << spacecount << "\n";
+    }
+
+    cout << spacecount+1;
+}
