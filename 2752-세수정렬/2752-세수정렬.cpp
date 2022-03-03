f7058f0208628f0daf60515094ad29d52d671d03
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-03 21:04:03 +0900

    add

---
+++ 2752-세수정렬/2752-세수정렬.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int a[3];
+    cin >> a[0]>>a[1]>>a[2];
+    sort(a,a+3);
+    cout <<a[0]<<" "<<a[1]<< " " <<a[2];
+}
