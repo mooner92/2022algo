6ea12845257544a108b54986e59ad618bfaf3ba9
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-03 22:35:09 +0900

    add

---
+++ 20353-Atrium/20353-Atrium.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+using namespace std;
+
+int main(){
+    double a;
+    cin >> a;
+    printf("%.8f",sqrt(a)*4);     //10의 -6제곱까지는 나타내주어야 정답처리가 됨
+}
