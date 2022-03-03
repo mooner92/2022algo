c539136bca32b81989a6f171b736c26a7d2e99b8
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-03 22:59:53 +0900

    add

---
+++ 4635-speedlimit/4635-speedlimit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int repeat=1;
+    while(repeat!=-1){
+        cin >> repeat;
+        int sum=0,stan=0;
+        for(int i=0;i<repeat;i++){
+            int a,b;
+            cin>>a>>b;
+            sum+=(a*(b-stan));
+            stan=b;
+        }
+        if(repeat==-1) break;
+        cout << sum << " miles"<<endl;
+
+    }
+}
