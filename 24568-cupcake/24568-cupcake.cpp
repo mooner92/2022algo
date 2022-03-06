1c229927e6371b0a07ce2d7f3b313948d4896c0f
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-06 12:48:38 +0900

    add

--- 24568-cupcake/24568-cupcake.cpp
+++ 24568-cupcake/24568-cupcake.cpp
@@ -3,5 +3,8 @@ using namespace std;
 int main(){
     int a,b;
     cin >>a>>b;
-    cout << 28
+    if(28<((a*8)+(b*3))){
+        cout << ((a*8)+(b*3))-28;
+    }
+    else cout <<"0";
 }
---
+++ 8437-JULKA/8437-JULKA.py
@@ -0,0 +1,4 @@
+a = int(input())
+b = int(input())
+print((a+b)//2)
+print((a-b)//2)
