65299ce793db4327a7240070e460c8b48dc46925
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-07 21:25:41 +0900

    add

--- 2292-벌집/2292-벌집.cpp
+++ 2292-벌집/2292-벌집.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    int k,stan=1,sum=2;
+    cin>>k;
+    if(k==1){
+            cout <<"1";
+            return 0;
+        }
+    for(int i=0;i<1000000;i++){
+        
+        if(sum>k){
+            cout << stan;
+            break;
+        }
+        stan++;
+        sum+=(6*(i+1));
+
+    }
 }
