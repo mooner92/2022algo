61f146759539ad12848f3e63d2dcdc3107e68c4c
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-08 21:02:55 +0900

    add

---
+++ recursion/resursion.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void countNum(int num)
+{
+  if (num == 1)
+  {
+    cout << "Num : " << num << endl;  //재귀함수는 탈출조건이 있어야함
+    return;
+  }
+  else
+  {
+    cout << "Num : " << num << endl;
+    countNum(num - 1);
+  }
+}
+
+
+int main()
+{
+  countNum(5);
+  return 0;
+}
+
+
+//출처: https://see-ro-e.tistory.com/21 [SeeRoE 프로그래밍 기록]
