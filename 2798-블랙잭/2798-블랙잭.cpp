fa2190c8f52e49b70de2e03a3042fcbf7e2559b5
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-09 17:40:39 +0900

    add

--- 2798-블랙잭/2798-블랙잭.cpp
+++ 2798-블랙잭/2798-블랙잭.cpp
@@ -10,7 +10,27 @@ int main(){
         cin >>a;
         k.push_back(a);  //벡터로 n칸짜리 배열 생성후 수 입력
     }
-    int b = k.capacity();
+    int b = k.size();
+    int min=300000;
+    for(int i=0;i<b;i++){
+        for(int j=0;j<b;j++){
+            if(i==j){    //같으면 다음루프로
+                break;
+            }
+            for(int h=0;h<b;h++){
+                if(j==h || i==h){  //하나라도 같으면 다음루프로
+                    break;
+                }
+                int value=k[i]+k[j]+k[h];
+                int r = m-value;
+                    if(r>=0){
+                    if(r<min){
+                    min=r;
+                    }
+                }
+            }
+        }
+    }
+    cout <<m-min;
     
-    int h[b][b][b];
 }
