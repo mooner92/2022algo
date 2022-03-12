86d7008a18eb4fb349a5dfe34692fda58c88ad47
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-12 19:13:18 +0900

    add

---
+++ 15829-hashing/15829-hashing.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a,sum=0;
+    string s;
+    cin>>a;
+    cin>>s;
+    for(int i=0;i<a;i++){
+
+    }
+
+    
+
+    
+}
