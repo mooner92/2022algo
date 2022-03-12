acf6a9d00b358b76421657cd37ba3eb2d1744d4b
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-12 22:36:39 +0900

    add

--- 15829-hashing/15829-hashing.cpp
+++ 15829-hashing/15829-hashing.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 int main()
 {
@@ -7,13 +8,16 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int a,sum=0;
+    int a;
     string s;
     cin>>a;
     cin>>s;
+    long long sum=0;
     for(int i=0;i<a;i++){
-
+        sum+=((s[i]-96)*pow(31,i));
     }
+
+    cout <<sum;
 
     
 
