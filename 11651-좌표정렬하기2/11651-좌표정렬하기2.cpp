14dc79555ce81fc60eb542967854c79151032ed1
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-15 14:00:01 +0900

    add

---
+++ 11651-좌표정렬하기2/11651-좌표정렬하기2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int tc,t;
+    cin>>tc;
+    vector<int> k;
+    vector<pair<int,int>> g;
+    for(int i=0;i<tc;i++){
+        int q,w;
+        cin>>q>>w;
+        g.push_back(make_pair(q,w));  //[q,w]로 이루어진 페어벡터를 생성
+    }
+    sort(g.begin(),g.end());
+    for(int i=0;i<tc;i++){
+        cout << g[i].first <<" "<<g[i].second<<"\n";
+    }
+}
