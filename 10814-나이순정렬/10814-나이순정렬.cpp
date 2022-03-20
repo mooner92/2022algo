550d1d6abc6bdc77c5233b5665e26ab58f601669
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-20 18:10:38 +0900

    add

--- 10814-나이순정렬/10814-나이순정렬.cpp
+++ 10814-나이순정렬/10814-나이순정렬.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
+
+bool cmp (pair<int, string> u, pair<int, string> v)
+{
+    return u.first < v.first;    //u<v일 때 1반환 
+}
+
 int main(){
     int tc;
     cin>>tc;
+    vector<pair<int,string>> v;
     for(int i=0;i<tc;i++){
-        vector<pair<int,string>> v;
         int q;
         string s;
         cin>>q>>s;
         v.push_back(make_pair(q,s));
-        //가입한 순으로 정렬하는 코드 작성
+    }
+    stable_sort(v.begin(),v.end(),cmp);    //sort 사용시 문자까지 정렬됨   cmp를 따로 만들어 줌으로써 first항으로만 정렬하게끔 함 
+    //sort로 하면 오류남 stable_sort 사용시 잘 됨
+    for(int i=0;i<v.size();i++){
+        cout <<v[i].first<<" "<<v[i].second<<"\n";
     }
 }
