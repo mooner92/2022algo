197fe659eba72cd298e2ccf308e03491622d9144
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-21 16:43:29 +0900

    add

---
+++ 10773-제로/10773-제로2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main(){
+    int tc;
+    cin>>tc;
+    stack<int> s;
+    int sum=0;
+    for(int i=0;i<tc;i++){
+        int q;
+        cin>>q;
+        if(q!=0)
+            s.push(q);
+        else{
+            s.pop();
+        } 
+    }
+    int k=s.size();
+    for(int i=0;i<k;i++){
+        sum+=s.top();
+        s.pop();
+    }
+    cout<<sum;
+}
+
+//s.size()랑 
+//k = s.size() 랑은 값차이가 남
---
+++ 10773-제로/10773-제로3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    int k, n;
+    cin >> k;
+    stack<int> s;
+    for (int i = 0; i < k; i++) {
+        cin >> n;
+        if (n != 0) {
+            s.push(n);
+        } else {
+            s.pop();
+        }
+    }
+    int sum = 0;
+    int size = s.size();
+    for (int i = 0; i < size; i++) {
+        sum += s.top();
+        s.pop();
+    }
+    cout << sum;
+    return 0;
+}
+
