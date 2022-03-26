0c26392e4cdca299fd7242820f7c0cfbbc5bf796
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-26 18:36:29 +0900

    add

---
+++ 2805-나무자르기/2805-나무자르기.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll=long long;
+vector<int> k;
+int main(){
+    ll n,h;
+    cin>>n>>h;
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        k.push_back(a);
+    }  //vector작성
+    sort(k.begin(),k.end());
+    while(1){
+        int half = k.size()/2;  //바이너리서치의 피봇
+        if(k[half]
+    }
+    
+
+
+}
