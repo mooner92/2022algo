b0454d7437025630e7365af75de1e30911576099
Mooner92 <chlaudgjs0989@gmail.com>
2022-04-07 01:33:27 +0900

    add

--- 18111-마인크래프트/18111-마인크래프트.cpp
+++ 18111-마인크래프트/18111-마인크래프트.cpp
@@ -2,13 +2,20 @@
 #include <vector>
 using namespace std;
 using ll=long long;
-ll a[250001];
+//ll a[250001];
 int main(){
     ll N,M,B;
     ll dig_T=0,stack_T=0;
+    vector<ll> v;
     cin>>N>>M>>B;
     for(int i=0;i<N*M;i++){
-        cin>> a[i];
-    }//배열에 전부 담음
+        ll temp;
+        cin>> temp;
+        v.push_back(temp);
+    }//배열에 전부 담음 || v에 전부 담음
+    sort(v.begin(),v.end());  //sort 오름차순
+    
+
+
 
 }
