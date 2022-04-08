fa2eb231b46a78db8a6812916dd503eaa13a14d5
Mooner92 <chlaudgjs0989@gmail.com>
2022-04-08 21:37:06 +0900

    add

--- example.cpp
+++ example.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 int main(){
-    /*string x;
+    string x;
     getline(cin,x);
     cout << x.size() <<"\n";
     cout << x.length();     //같은 값이 나온다...
     double a=10.3;
-    cout << a/4.0;*/
+    cout << a/4.0;
 
-/*double a=63.99;
+double a=63.99;
 cout << sqrt(a) << endl;
 int k =a;
 int c = sqrt(a);
