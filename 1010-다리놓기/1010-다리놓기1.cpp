f8edb42b835b617f315dc9f94d37d69d394d64ef
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-06 02:39:01 +0900

    add

---
+++ 1010-다리놓기/1010-다리놓기1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int tc;
+    cin >> tc;
+    for (int i = 0; i < tc; i++)
+    {
+        int n, m;
+        cin >> n >> m;
+        int dummy = m - n;
+    }
+}
