cc1b05f3a22592507744ee2cc0474aeef642350c
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-05 23:40:05 +0900

    add

--- 1009-분산처리/1009-분산처리1.cpp
+++ 1009-분산처리/1009-분산처리1.cpp
@@ -9,9 +9,58 @@ int main()
         int q, w;
         cin >> q, w;
         q %= 10;
+        // cout << q << endl;
         if (q == 2 || q == 3 || q == 7 || q == 8)
         {
-            switch (w % 4) // 4항계산
+            int t = w % 4;
+            if (t == 1)
+                cout << q << "\n";
+            else if (t == 2)
+            {
+                if (q == 2 || q == 8)
+                    cout << 4 << "\n";
+                else if (q == 3 || q == 7)
+                    cout << 9 << "\n";
+            }
+            else if (t == 3)
+            {
+                int k;
+                (q == 2) ? k = 8 : (q == 3) ? k = 7
+                               : (q == 7)   ? k = 3
+                               : (q == 8)   ? k = 2
+                                            : k = 0;
+                cout << k << "\n";
+            }
+            else if (t == 0)
+            {
+                int j;
+                (q == 2 || 1 == 8) ? j = 6 : (q == 3 || q == 7) ? j = 1
+                                                                : j = 0;
+                cout << j << "\n";
+            }
+        }
+        else if (q == 1 || q == 5 || q == 6 || q == 10)
+        {
+            cout << q << "\n";
+        }
+        else if (q == 4)
+        {
+            int k;
+            (w % 2 == 1) ? k = 4 : k = 6;
+            cout << k << "\n";
+        }
+        else if (q == 9)
+        {
+            int k;
+            (w == 1) ? k = 9 : (w % 2 == 1) ? k = 2
+                                            : k = 8;
+            cout << k << "\n";
+        }
+    }
+}
+
+/*
+switch (w % 4) // 4항계산
             {
             case 1:
                 cout << q << endl;
@@ -27,35 +76,16 @@ int main()
                 (q == 2) ? k = 8 : (q == 3) ? k = 7
                                : (q == 7)   ? k = 3
                                : (q == 8)   ? k = 2
-                                            : true;
+                                            : false;
                 cout << k << endl;
                 break;
             case 0:
                 int j;
                 (q == 2 || 1 == 8) ? j = 6 : (q == 3 || q == 7) ? j = 1
-                                                                : true;
+                                                                : false;
                 cout << j << endl;
                 break;
             default:
-                break;
+                cout << "none";
             }
-        }
-        else if (q == 1 || q == 5 || q == 6 || q == 10)
-        {
-            cout << q << endl;
-        }
-        else if (q == 4)
-        {
-            int k;
-            (w % 2 == 1) ? k = 4 : k = 6;
-            cout << k << endl;
-        }
-        else if (q == 9)
-        {
-            int k;
-            (w == 1) ? k = 9 : (w % 2 == 1) ? k = 2
-                                            : k = 8;
-            cout << k << endl;
-        }
-    }
-}
+*/
