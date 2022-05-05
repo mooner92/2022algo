692bc4ca387ab6c1b70f6e49dcad6b3196deb415
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-06 02:26:20 +0900

    add

---
+++ 1010-다리놓기/1010-다리놓기.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+using ll = long long;
+int main()
+{
+    int tc;
+    cin >> tc;
+    for (int i = 0; i < tc; i++)
+    {
+        int n, m;
+        ll dummy = 0;
+        cin >> n >> m;
+        if (n == m)
+        {
+            cout << 1 << "\n";
+            continue;
+        }
+        if (n < (m - n))
+        {
+            dummy = n;
+        }
+        else
+            dummy = m - n;
+        ll ans = 1;
+        for (int j = m; j > dummy; j--)
+        {
+            ans *= j;
+        }
+        cout << ans << "\n";
+    }
+}
