60b38c23231f9a25e322c20e32905ede97bbae19
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-06 17:59:56 +0900

    add

---
+++ 1010-다리놓기/1010-다리놓기3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+using ll = long long;
+
+ll fact(int n)
+{
+}
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    for (int i = 0; i < tc; i++)
+    {
+        int n, m;
+        cin >> n >> m;
+    }
+}
