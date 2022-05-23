fd126502fe203e940170243e5f7e500c73aad7e5
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-23 15:30:53 +0900

    add

---
+++ 2941-크로아티아알파벳/2941-크로아티아알파벳.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    string s = "";
+    cin >> s;
+    int ans = 0;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == 'c')
+        {
+            if (s[i + 1] == '=' || s[i + 1] == '-')
+            {
+                i += 1;
+                ans += 1;
+            }
+            else
+                ans += 1;
+        }
+        else if (s[i] == 'd')
+        {
+            if (s[i + 1] == 'z')
+            {
+                if (s[i + 2] == '=')
+                {
+                    ans += 1;
+                    i += 2;
+                } /**/
+            }
+            else if (s[i + 1] == '-')
+            {
+                ans += 1;
+                i++;
+            }
+            else
+            {
+                ans += 1;
+            }
+        }
+        else if (s[i] == 'l')
+        {
+            if (s[i + 1] == 'j')
+            {
+                ans += 1;
+                i++;
+            }
+            else
+            {
+                ans += 1;
+            }
+        }
+        else if (s[i] == 'n')
+        {
+            if (s[i + 1] == 'j')
+            {
+                ans += 1;
+                i++;
+            }
+            else
+            {
+                ans += 1;
+            }
+        }
+        else if (s[i] == 's')
+        {
+            if (s[i + 1] == '=')
+            {
+                ans += 1;
+                i++;
+            }
+            else
+                ans += 1;
+        }
+        else if (s[i] == 'z')
+        {
+            if (s[i + 1] == '=')
+            {
+                ans += 1;
+                i++;
+            }
+            else
+                ans += 1;
+        }
+        else
+        {
+            ans += 1;
+        }
+    }
+    cout << ans;
+}
