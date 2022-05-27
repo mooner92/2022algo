38701609982538158abc89a31d84dd517e4c1a53
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-27 17:44:54 +0900

    add

--- 24725-MBTI/24725-MBTI.cpp
+++ 24725-MBTI/24725-MBTI.cpp
@@ -5,6 +5,7 @@ int main()
 {
     int n, m;
     cin >> n >> m;
+
     vector<string> v;
     for (int i = 0; i < n; i++)
     {
@@ -21,10 +22,37 @@ int main()
     cout << v[0][0];
 }
 
-void search4x4(vector<string> v1, int i1, int j1, int n1, int m1)
+int search4x4(vector<string> v1, int i1, int j1, int n1, int m1)
 {
-    // case1 width
-    for (int i = 0; i < n1; i++)
+    // width,height
+    int cnt = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        // case1 width
+        if (v1[i1 + i][j1] == 'E' || v1[i1 + i][j1] == 'I')
+            if (v1[i1 + i][j1 + 1] == 'N' || v1[i1 + i][j1 + 1] == 'S')
+                if (v1[i1 + i][j1 + 2] == 'T' || v1[i1 + i][j1 + 2] == 'F')
+                    if (v1[i1 + i][j1 + 3] == 'P' || v1[i1 + i][j1 + 3] == 'J')
+                        cnt++;
+                    else if (v1[i1 + i][j1] == 'P' || v1[i1 + i][j1] == 'J')
+                        if (v1[i1 + i][j1 + 1] == 'T' || v1[i1 + i][j1 + 1] == 'F')
+                            if (v1[i1 + i][j1 + 2] == 'N' || v1[i1 + i][j1 + 2] == 'S')
+                                if (v1[i1 + i][j1 + 3] == 'E' || v1[i1 + i][j1 + 3] == 'I')
+                                    cnt++;
+        // case2 height
+        if (v1[i1][j1 + i] == 'E' || v1[i1][j1] == 'I')
+            if (v1[i1 + 1][j1 + i] == 'N' || v1[i1 + 1][j1 + i] == 'S')
+                if (v1[i1 + 2][j1 + i] == 'T' || v1[i1 + 2][j1 + i] == 'F')
+                    if (v1[i1 + 3][j1 + i] == 'P' || v1[i1 + 3][j1 + i] == 'J')
+                        cnt++;
+                    else if (v1[i1][j1 + i] == 'P' || v1[i1][j1 + i] == 'J')
+                        if (v1[i1 + 1][j1 + i] == 'T' || v1[i1 + 1][j1 + i] == 'F')
+                            if (v1[i1 + 2][j1 + i] == 'N' || v1[i1 + 2][j1 + i] == 'S')
+                                if (v1[i1 + 3][j1 + i] == 'E' || v1[i1 + 3][j1 + i] == 'I')
+                                    cnt++;
+    }
+    // case 3 diagonal
+    for (int i = 0; i < 2; i++)
     {
     }
 }
