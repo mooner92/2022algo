10019a8aefc7530e216de1dd466a954e29eb8af2
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-27 12:38:44 +0900

    add

--- 2851-슈퍼마리오/2851-슈퍼마리오.cpp
+++ 2851-슈퍼마리오/2851-슈퍼마리오.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
+    int s[10] = {0};
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> s[i];
+    }
+    int sum1 = 0, sum2 = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        sum2 += s[j];
+        if (s[j] >= 100)
+        {
+            sum1 = sum2 - s[j];
+            break;
+        }
+    }
+    int ans = 0;
+    if (abs(100 - sum1) > (sum2 - 100))
+    {
+        ans = sum2;
+    }
+    else
+    {
+        ans = sum1;
+    }
+    cout << ans;
 }
