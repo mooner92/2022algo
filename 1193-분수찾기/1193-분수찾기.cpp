56303e0c6ce088685e4c591e46b8c075ec34380b
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-30 15:35:18 +0900

    add

--- 1193-분수찾기/1193-분수찾기.cpp
+++ 1193-분수찾기/1193-분수찾기.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 using namespace std;
-//재귀풀이
-
-int rec(int n)
-{
-}
 
 int main()
 {
-    int n = 0;
+    int n, cnt = 0, p = 0, a = 0, b = 0;
     cin >> n;
+    while (p != n)
+    {
+        cnt++;
+        for (int i = cnt - 1; i >= 1; i--)
+        {
+            if (p == n)
+                break;
+            a = cnt - i;
+            b = i;
+            p++;
+        }
+        cnt++;
+        for (int i = 1; i < cnt; i++)
+        {
+            if (p == n)
+                break;
+            a = cnt - i;
+            b = i;
+            p++;
+        }
+    }
+
+    cout << a << "/" << b;
 }
