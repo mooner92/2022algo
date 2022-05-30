ef1a2e29d67b79495ff7713c3627dfc8ff375b57
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-30 15:08:05 +0900

    add

---
+++ 1193-분수찾기/1193-분수찾기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+//재귀풀이
+
+int rec(int n)
+{
+}
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+}
---
+++ 17202-번호궁합/17202-번호궁합.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 0, m = 0;
+}
+int ans(int n, int m)
+{
+}
