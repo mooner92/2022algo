a94f66d8913c9d5d6053488f62dd2f6adce43b9d
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-30 16:24:38 +0900

    add

--- 17202-번호궁합/17202-번호궁합.cpp
+++ 17202-번호궁합/17202-번호궁합.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+string phone1, phone2;
+vector<int> v;
+void init()
+{
+    cin >> phone1 >> phone2;
+    for (int i = 0; i < 8; i++)
+    {
+        // ASCII CODE '0' is 48
+        v.push_back(int(phone1[i]) - 48);
+        v.push_back(int(phone2[i]) - 48);
+    }
+}
+
+void solve(vector<int> v)
+{
+    if (v.size() == 2)
+    {
+        cout << v[0] << v[1];
+        return;
+    }
+    vector<int> v2;
+    for (int i = 1; i < v.size(); i++)
+    {
+        v2.push_back((v[i - 1] + v[i]) % 10);
+    }
+    solve(v2);
+}
+
 int main()
 {
-    int n = 0, m = 0;
-}
-int ans(int n, int m)
-{
+    init();
+    solve(v);
 }
