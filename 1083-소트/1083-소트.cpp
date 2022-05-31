d906185e41cda313f4d53a1ca6dc4b17fdee0678
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-31 13:48:07 +0900

    add

---
+++ 1083-소트/1083-소트.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n, s;
+    vector<int> v1;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        v1.push_back(a);
+    }
+}
