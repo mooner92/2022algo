032a67bb7540c96c80347e7e43701dd28e7ac7e0
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-31 13:20:09 +0900

    add

---
+++ 6986-절사평균/6986-절사평균1.cpp
@@ -0,0 +1,40 @@
+// #include <bits/stdc++.h>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+typedef long long ll;
+#define endl '\n'
+int main()
+{
+    int n, k;
+    vector<ll> arr;
+    cin >> n >> k;
+    arr.resize(n);
+    for (int i = 0; i < n; i++)
+    {
+        double tmp;
+        cin >> tmp;
+        arr[i] = (ll)(tmp * 1000);
+    }
+    sort(arr.begin(), arr.end());
+    ll s = 0;
+    for (int i = k; i + k < arr.size(); i++)
+    {
+        s += arr[i];
+    }
+    ll ans = s / (arr.size() - k - k) + 5;
+    printf("%lld.%02lld\n", ans / 1000, ans % 1000 / 10);
+    s = 0;
+
+    s += k * arr[k];
+    for (int i = k; i + k < arr.size(); i++)
+    {
+        s += arr[i];
+    }
+    s += k * arr[arr.size() - 1 - k];
+    ans = s / arr.size() + 5;
+    printf("%lld.%02lld\n", ans / 1000, ans % 1000 / 10);
+}
