839178f8bc371d180cf6e09cfc0dbbb394f52543
Mooner92 <chlaudgjs0989@gmail.com>
2022-06-03 13:24:16 +0900

    add

--- 24568-cupcake/24568-cupcake.cpp
+++ 24568-cupcake/24568-cupcake.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int a,b;
-    cin >>a>>b;
-    if(28<((a*8)+(b*3))){
-        cout << ((a*8)+(b*3))-28;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (28 < ((a * 8) + (b * 3)))
+    {
+        cout << ((a * 8) + (b * 3)) - 28;
     }
-    else cout <<"0";
-}
+    else
+        cout << "0";
+} /**/
