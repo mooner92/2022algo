5a9c04f298feee41d73443a3c9ac561a8e009aea
Mooner92 <chlaudgjs0989@gmail.com>
2022-06-04 23:06:42 +0900

    add

--- 1065-한수/1065-한수.cpp
+++ 1065-한수/1065-한수.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
 using namespace std;
-int main(){
-    
+
+int searchAns(int h)
+{ // h만큼 반복
+    int cnt = 0;
+    for (int i = 1; i <= h; i++)
+    { // 1~h까지 반복
+        if (i < 100)
+        {
+            cnt++;
+        }
+        else if (i >= 100 && i < 1000)
+        {
+            int a, b, c;
+            c = i % 10;
+            i /= 10;
+            b = i % 10;
+            i /= 10;
+            a = i;
+            if (a == b && b == c)
+                cnt++;
+            else if (a + c == b * 2)
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main()
+{
+    int s;
+    cin >> s;
+    int ans = searchAns(s);
+    cout << ans;
 }
