4a088a9613c413acfb1846fce282995875b26d01
Mooner92 <chlaudgjs0989@gmail.com>
2022-06-06 02:34:29 +0900

    add

---
+++ 1463-1로만들기/1463-1로만들기1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+}
+
+int search(int s)
+{
+    int cnt = 0;
+    while (s != 1)
+    {
+        if (s % 3 == 0)
+        {
+            s /= 3;
+            cnt++;
+        }
+        else if ()
+    }
+}
