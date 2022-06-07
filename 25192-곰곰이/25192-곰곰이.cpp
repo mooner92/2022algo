f654539b4e5aecbb2ccd9bd447acd98a28f0ffc4
Mooner92 <chlaudgjs0989@gmail.com>
2022-06-07 21:47:49 +0900

    add

---
+++ 25192-곰곰이/25192-곰곰이.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<string> s;
+    vector<bool> snap(n, -1);
+    for (int i = 0; i < n; i++)
+    {
+        string dummy1;
+        cin >> dummy1;
+        if (dummy1 == "ENTER")
+        {
+                }
+    }
+}
