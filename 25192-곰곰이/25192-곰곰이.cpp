53c03a83774a84752c1371da6c22886001bbb372
Mooner92 <chlaudgjs0989@gmail.com>
2022-06-09 16:27:11 +0900

    add

--- 25192-곰곰이/25192-곰곰이.cpp
+++ 25192-곰곰이/25192-곰곰이.cpp
@@ -6,14 +6,27 @@ int main()
     int n;
     cin >> n;
     vector<string> s;
-    vector<bool> snap(n, -1);
+    vector<bool> snap(n, false);
+    int p = 0; // enter의 경우를 제외한 pivot p를 선언하여 유효인덱스로 사용
+    int cnt = 0;
     for (int i = 0; i < n; i++)
     {
         string dummy1;
         cin >> dummy1;
         if (dummy1 == "ENTER")
         { // enter가 입력되면 해시테이블 -1로 초기화
-            fill(snap.begin(), snap.end(), -1);
+            fill(snap.begin(), snap.end(), false);
+        }
+        else
+        {
+            s.push_back(dummy1);
+            if (snap[p] == false)
+            {
+                cnt++;
+                snap[p] = true;
+            }
+            p++;
         }
     }
+    cout << cnt;
 }
