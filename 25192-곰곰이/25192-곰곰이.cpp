2dcdc6330ba5e4493aba04a894bf14e5bba42aed
Mooner92 <chlaudgjs0989@gmail.com>
2022-06-09 16:45:16 +0900

    add

--- 25192-곰곰이/25192-곰곰이.cpp
+++ 25192-곰곰이/25192-곰곰이.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+bool findI(vector<string> s1, string str)
+{ // s에서 dummy1찾기
+    for (int i = 0; i < s1.size(); i++)
+    {
+        if (s1[i] == str)
+        {
+            return false;
+        }
+        else
+            return true;
+    }
+}
+
 int main()
 {
     int n;
     cin >> n;
-    vector<string> s;
+    vector<string> s(n, "");
     vector<bool> snap(n, false);
     int p = 0; // enter의 경우를 제외한 pivot p를 선언하여 유효인덱스로 사용
     int cnt = 0;
@@ -14,17 +28,18 @@ int main()
         string dummy1;
         cin >> dummy1;
         if (dummy1 == "ENTER")
-        { // enter가 입력되면 해시테이블 -1로 초기화
+        { // enter가 입력되면 해시테이블-1로 초기화, 메인테이블은 빈칸으로 초기화
             fill(snap.begin(), snap.end(), false);
+            fill(s.begin(), s.end(), "");
+            p = 0;
         }
         else
         {
-            s.push_back(dummy1);
-            if (snap[p] == false)
+            if (findI(s, dummy1))
             {
                 cnt++;
-                snap[p] = true;
             }
+            s.push_back(dummy1);
             p++;
         }
     }
