7e0dd29e3488ca7e961a58ae4c69212df59d6a68
Mooner92 <chlaudgjs0989@gmail.com>
2022-06-14 17:43:55 +0900

    add

--- 10773-제로/10773-제로3.cpp
+++ 10773-제로/10773-제로3.cpp
@@ -2,25 +2,30 @@
 #include <stack>
 using namespace std;
 
-int main() {
+int main()
+{
     int k, n;
     cin >> k;
     stack<int> s;
-    for (int i = 0; i < k; i++) {
+    for (int i = 0; i < k; i++)
+    {
         cin >> n;
-        if (n != 0) {
+        if (n != 0)
+        {
             s.push(n);
-        } else {
+        }
+        else
+        {
             s.pop();
         }
     }
     int sum = 0;
     int size = s.size();
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         sum += s.top();
         s.pop();
     }
     cout << sum;
     return 0;
 }
-
