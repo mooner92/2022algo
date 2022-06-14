aeedb0c2b6e127dc5a520a89c94b39b12b529902
Mooner92 <chlaudgjs0989@gmail.com>
2022-06-14 17:42:54 +0900

    add

--- 20492-세금/20492-세금.cpp
+++ 20492-세금/20492-세금.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int a;
     cin >> a;
-    cout << a*78/100 <<" "<< a-(a/5*22/100);
+    cout << a * 78 / 100 << " " << a - (a / 5 * 22 / 100);
 }
