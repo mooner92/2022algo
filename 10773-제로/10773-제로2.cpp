220bda9c04f41e609c80c82f841f0c44dd5eed20
Mooner92 <chlaudgjs0989@gmail.com>
2022-06-14 17:43:30 +0900

    add

--- 10773-제로/10773-제로2.cpp
+++ 10773-제로/10773-제로2.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <stack>
 using namespace std;
-int main(){
+int main()
+{
     int tc;
-    cin>>tc;
+    cin >> tc;
     stack<int> s;
-    int sum=0;
-    for(int i=0;i<tc;i++){
+    int sum = 0;
+    for (int i = 0; i < tc; i++)
+    {
         int q;
-        cin>>q;
-        if(q!=0)
+        cin >> q;
+        if (q != 0)
             s.push(q);
-        else{
+        else
+        {
             s.pop();
-        } 
+        }
     }
-    int k=s.size();
-    for(int i=0;i<k;i++){
-        sum+=s.top();
+    int k = s.size();
+    for (int i = 0; i < k; i++)
+    {
+        sum += s.top();
         s.pop();
     }
-    cout<<sum;
+    cout << sum;
 }
 
-//s.size()랑 
-//k = s.size() 랑은 값차이가 남
+// s.size()랑
+// k = s.size() 랑은 값차이가 남
