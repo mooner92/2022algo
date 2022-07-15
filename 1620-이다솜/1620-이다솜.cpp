252b85ee8c7b7d4603876d1e1a01a9d9d3403bb7
Mooner92 <chlaudgjs0989@gmail.com>
2022-07-15 16:06:11 +0900

    add

--- 1620-이다솜/1620-이다솜.cpp
+++ 1620-이다솜/1620-이다솜.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
+
 int main()
 {
-    int ptc = 0, itc = 0;
-    cin >> ptc >> itc;
-    for (int i = 0; i < ptc; i++)
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false); //없으면 시간 초과
+    int n = 0, m = 0;
+    cin >> n >> m;
+    string strarr[100001];
+    unordered_map<string, int> p;
+    for (int i = 1; i <= n; i++)
     {
+        string s;
+        cin >> s;
+        p.insert(make_pair(s, i));
+        strarr[i] = s;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        string s1;
+        cin >> s1;
+        if (p.find(s1) != p.end())
+        {
+            cout << p.find(s1)->second << "\n";
+        }
+        else
+        {
+            int j = stoi(s1);
+            cout << strarr[j] << "\n";
+        }
     }
 }
