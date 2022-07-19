ba96461a859d9c96f11b0bbd649365409fe0ed86
Mooner92 <chlaudgjs0989@gmail.com>
2022-07-19 19:54:41 +0900

    add

---
+++ 11720-숫자의합/11720-숫자의합.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    char *ptr;
+    int sum = 0;
+    scanf("%d", &n);
+    getchar();
+    scanf("%s", ptr);
+    for (int i = 0; i < n; i++)
+    {
+        sum += ((*ptr + i) - ('0'));
+    }
+    printf("%d", sum);
+    return 0;
+}
---
+++ 1764-듣보잡/1764-듣보잡.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<string> s;
+    for (int i = 0; i < n; i++)
+}
