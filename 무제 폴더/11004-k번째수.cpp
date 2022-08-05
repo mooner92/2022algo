bf53dc05513de9e01197f8a1f5ef398d46b5efe6
Mooner92 <chlaudgjs0989@gmail.com>
2022-08-05 18:45:46 +0900

    add

---
+++ 무제 폴더/11004-k번째수.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, p, tmp;
+    vector<int> v;
+    cin >> n >> p;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+    sort(v.begin(), v.end());
+    cout << v[p - 1] << endl;
+}
