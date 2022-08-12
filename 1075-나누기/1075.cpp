004c67883e9f149b8e55feae87c657dab1826dd6
Mooner92 <chlaudgjs0989@gmail.com>
2022-08-12 21:41:41 +0900

    add

--- 1075-나누기/1075.cpp
+++ 1075-나누기/1075.cpp
@@ -13,3 +13,24 @@ int main()
     }
     printf("%02d", pq.top() * (-1));
 }
+/*
+#include<stdio.h>
+int main(){
+    int n,f,temp,i;
+
+    scanf("%d %d", &n, &f);
+
+    n = (n/100) * 100;
+
+    for(i = 0; i < 100; i++){
+        temp = n;
+        if( (temp+=i) % f==0)
+            break;
+    }
+
+    if(i<10)
+        printf("0");
+
+        printf("%d", i);
+}
+*/
