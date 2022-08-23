226bc0774cb2866a243a01cd8c3576c5e1b7fafd
Mooner92 <chlaudgjs0989@gmail.com>
2022-08-23 18:08:01 +0900

    add

--- 11723-집합/11723-집합.cpp
+++ 11723-집합/11723-집합.cpp
@@ -1,33 +1,83 @@
 #include <iostream>
 #include <string>
 //도수분포표를 사용한 풀이
+//캡슐화 최대한으로 이용
 using namespace std;
 
 string s1[7] = {"add", "remove", "check", "toggle", "empty", "all"};
 
-void process(string s, int k)
+int f[22] = {0};
+
+void p_add(int n)
 {
+    f[n]++;
+}
+void p_remove(int n)
+{
+    f[n] = 0;
+}
+void p_check(int n)
+{
+    if (f[n])
+    {
+        printf("1\n");
+    }
+    else
+        printf("0\n");
+}
+void p_toggle(int n)
+{
+    if (f[n])
+        f[n] = 0;
+    else
+        f[n]++;
+}
+void p_empty()
+{
+    for (int i = 1; i <= 20; i++)
+        f[i] = 0;
+}
+void p_all()
+{
+    for (int i = 1; i <= 20; i++)
+    {
+        f[i] = 1;
+    }
 }
 
 int main()
 {
-    int f[22] = {
-        0,
-    };
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     string s;
     int n, k;
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        scanf("%s", s);
-        if (s == s1[5]) // all이 아닌 경우
+        cin >> s;
+        if (s == s1[5])
+            p_all();
+        else if (s == s1[4])
+            p_empty();
+        else if (s == s1[3])
         {
-            for (int j = 1; j < 21; j++)
-            {
-                f[j] = 1;
-            }
-            continue;
+            cin >> k;
+            p_toggle(k);
         }
-        process(s, k);
+        else if (s == s1[2])
+        {
+            cin >> k;
+            p_check(k);
+        }
+        else if (s == s1[1])
+        {
+            cin >> k;
+            p_remove(k);
+        }
+        else if (s == s1[0])
+        {
+            cin >> k;
+            p_add(k);
+        }
     }
 }
