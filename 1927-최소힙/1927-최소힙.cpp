815bad94a47dfc6a8f34b16422a6ee748d1a657f
Mooner92 <chlaudgjs0989@gmail.com>
2022-09-02 16:20:50 +0900

    add

---
+++ 1927-최소힙/1927-최소힙.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+//#include <functional>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long n, k;
+    priority_queue<long long, vector<long long>, greater<long long>> pql;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> k;
+        pql.push(k);
+        if (k == 0)
+        {
+            if (pql.empty())
+                cout << "0\n";
+            else
+            {
+                cout << pql.top() << "\n";
+                pql.pop();
+            }
+        }
+    }
+}
