47d5b3ff2b6fba20e28204d28f6515331bb2178c
Mooner92 <chlaudgjs0989@gmail.com>
2022-09-05 15:50:17 +0900

    add

---
+++ 11724-연결요소의개수/11724-연결요소의개수.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    vector<vector<int>> v1;
+    int n, m;
+}
---
+++ 1927-최소힙/1927-최소힙1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    long long k;
+    priority_queue<long long> pq;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> k;
+        if (k == 0)
+        {
+            if (pq.empty())
+            {
+                cout << 0 << "\n";
+                continue;
+            }
+            cout << pq.top() * -1 << "\n";
+            pq.pop();
+        }
+        else
+        {
+            pq.push(-1 * k);
+        }
+    }
+}
