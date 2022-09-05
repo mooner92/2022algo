c617dc05f50d41b39ab7e78f23e567015ff757cb
Mooner92 <chlaudgjs0989@gmail.com>
2022-09-05 14:24:38 +0900

    add

---
+++ 1149-RGB거리/1149-RGB거리.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int rgb[1002][3] = {
+        0,
+    };
+    int n, r, g, b;
+    const int p1 = 0, p2 = 1, p3 = 2;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> r >> g >> b;
+        rgb[i][p1] = min(rgb[i - 1][0] + g, rgb[i - 1][0] + b);
+        rgb[i][p2] = min(rgb[i - 1][1] + r, rgb[i - 1][1] + b);
+        rgb[i][p3] = min(rgb[i - 1][2] + r, rgb[i - 1][2] + g);
+    }
+    cout << min(min(rgb[n][0], rgb[n][1]), rgb[n][2]);
+}
