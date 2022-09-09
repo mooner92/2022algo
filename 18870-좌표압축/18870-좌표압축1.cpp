fd38b850a923838a5f323ad25bca44c320e4208b
Mooner92 <chlaudgjs0989@gmail.com>
2022-09-09 14:54:58 +0900

    add

--- 18870-좌표압축/18870-좌표압축1.cpp
+++ 18870-좌표압축/18870-좌표압축1.cpp
@@ -3,14 +3,36 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 using namespace std;
 int main()
 {
-    int n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, k;
     vector<int> v1;
     bool visited[INT_MAX] = {
         0,
     };
     long long max = -1 * 1e9;
     long long min = 1e9;
-}
+    long long maxP = 0;
+    long long minP = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    { //중복 빼고 다 담아줌
+        cin >> k;
+        if (!visited[k - 1])
+        { // idx = k-1로 지정
+            visited[k - 1] = true;
+            v1.push_back(k);
+        }
+    }
+    vector<int> v2(v1);
+    sort(v1.begin(), v1.end());
+    for (int i = 0; i < n; i++)
+    {
+    }
+
+} //
