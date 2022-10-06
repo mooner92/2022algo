e0917d623bc3621f811be1f9336c63a63cb0cf92
Mooner92 <chlaudgjs0989@gmail.com>
2022-10-06 16:18:49 +0900

    add

---
+++ 2178-미로탐색/2178-미로탐색.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+bool visited[101][101];
+int ds[101][101];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, m;
+    cin >> n >> m;
+
+}
