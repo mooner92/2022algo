7ebc64fc970ba48656b9f952de99ca33a340a449
mooner92 <chlaudgjs0989@gmail.com>
2022-11-29 09:16:41 +0900

    add

---
+++ test_ex1_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int k;
+    int *p = &k;
+    scanf("%d",p);
+    while((*p)--){
+        //printf("%d",(*p)--);
+        printf("*");
+    }
+}
---
+++ test_ex1_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int arr[10];
+    int *p = arr;
+    for(int i=0;i<10;i++){
+        scanf("%d",&p[i]);
+        if(p[i]%2==0){
+            p[i]/=2;
+        }
+        else p[i]*=2;
+    }
+    for(int i=0;i<10;i++){
+        printf("%d",p[i]);
+    }
+}
