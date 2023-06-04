b10a28c0be084983bd5d18fc57d1a3fac3fcfe0b
Mooner92 <chlaudgjs0989@gmail.com>
2023-06-04 18:27:16 +0900

    add

---
+++ 5524-입실관리/ex1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n[102] = {0};
+    for (int i = 0; i < 100; i++)
+    {
+        n[i] = 7 * i;
+    }
+    for (int f : n)
+    {
+        cout << f << " " << f - 1 << '\n';
+    }
+}
