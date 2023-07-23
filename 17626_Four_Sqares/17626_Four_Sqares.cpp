a2afaed7b19e476565a8b107f4fb13bdfc3b6444
Mooner92 <chlaudgjs0989@gmail.com>
2023-07-23 17:36:32 +0900

    add

---
+++ 17626_Four_Sqares/17626_Four_Sqares.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int min_square_sum(int n)
+{
+    vector<int> dp(n + 1, 2147000000);
+    dp[0] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int j = 1;
+        while (j * j <= i)
+        {
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
+            j++;
+        }
+    }
+
+    return dp[n];
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << min_square_sum(n) << "\n";
+
+    return 0;
+}
