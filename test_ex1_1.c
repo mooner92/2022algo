c900f6f139ecb83a4688b748449b00aaeb3551d3
Mooner92 <54096383+mooner92@users.noreply.github.com>
2023-10-07 01:12:16 +0900

    add

--- test_ex1_1.c
+++ test_ex1_1.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int k;
     int *p = &k;
-    scanf("%d",p);
-    while((*p)--){
-        //printf("%d",(*p)--);
+    scanf("%d", p);
+    while ((*p)--)
+    {
+        // printf("%d",(*p)--);
         printf("*");
     }
 }
+//////
