57a6e6ead5778a4ac08378c1133dfb4de49a2ff5
Mooner92 <54096383+mooner92@users.noreply.github.com>
2023-10-13 13:21:44 +0900

    add

--- 1966-프린터큐/1966-프린터큐1.cpp
+++ 1966-프린터큐/1966-프린터큐1.cpp
@@ -12,38 +12,45 @@ int main()
 
 	scanf("%d", &testn);
 
-	for (int i = 0; i < testn; i++) {
+	for (int i = 0; i < testn; i++)
+	{
 		count = 0;
 		scanf("%d %d", &n, &m);
-		
+
 		priority_queue<int> pq; // 내림차순 정렬해야 pop할 때 높은게 빠져나옴.
 		queue<pair<int, int>> q;
 
-		for (int j = 0; j < n; j++) {
+		for (int j = 0; j < n; j++)
+		{
 			scanf("%d", &importance);
-			q.push({ j, importance });
+			q.push({j, importance});
 			pq.push(importance);
 		}
 
-		while (!q.empty()) {
+		while (!q.empty())
+		{
 			// 위치값과, 우선순위 값을 가져온 뒤 pop수행
 			int location = q.front().first;
 			int value = q.front().second;
 			q.pop();
-            
+
 			// 값 비교
-			if (pq.top() == value) {
+			if (pq.top() == value)
+			{
 				pq.pop();
 				++count;
-				if (m == location) {
+				if (m == location)
+				{
 					printf("%d\n", count);
 					break;
 				}
 			}
 
-			else q.push({ location, value });
+			else
+				q.push({location, value});
 		}
 	}
 
 	return 0;
 }
+////////
