eeb7fb6df92657b5ad5572a7a9346de334beb347
Mooner92 <54096383+mooner92@users.noreply.github.com>
2023-10-17 03:42:29 +0900

    add

---
+++ jungol_5805.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    long long X, Y;
+    cin >> X >> Y;
+
+    int Z = floor(Y * 100 / X);
+    int newZ = Z;
+    int tosses = 0;
+
+    while (Z == newZ && Z != 100)
+    {
+        X++;
+        Y++;
+        newZ = floor(Y * 100 / X);
+        tosses++;
+    }
+
+    if (Z == 100)
+    {
+        cout << -1 << endl; // Z값이 이미 100%이므로 Z값은 절대 바뀌지 않음
+    }
+    else
+    {
+        cout << tosses << endl; // Z값이 바뀌는 동안 필요한 동전 던지기 횟수
+    }
+
+    return 0;
+}
