f466b1d588e36857590d63f6355e42db18bc12f9
Mooner92 <54096383+mooner92@users.noreply.github.com>
2023-10-21 12:16:06 +0900

    add

--- test_ex1_2.c
+++ test_ex1_2.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int arr[10];
     int *p = arr;
-    for(int i=0;i<10;i++){
-        scanf("%d",&p[i]);
-        if(p[i]%2==0){
-            p[i]/=2;
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d", &p[i]);
+        if (p[i] % 2 == 0)
+        {
+            p[i] /= 2;
         }
-        else p[i]*=2;
+        else
+            p[i] *= 2;
     }
-    for(int i=0;i<10;i++){
-        printf("%d",p[i]);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d", p[i]);
     }
 }
+//////
