bd41cf69d2db38248ae7a885d4be6ebfb741aedf
Mooner92 <54096383+mooner92@users.noreply.github.com>
2023-10-30 12:34:47 +0900

    add

--- fibodp_1.cpp
+++ fibodp_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+/*
 int fibodp(int n)
 {
     if (n == 1 || n == 2)
@@ -9,6 +10,28 @@ int fibodp(int n)
 int main()
 {
     for (int i = 1; i <= 20; i++)
+    {
+        printf("%d항 : %d\n", i, fibodp(i));
+    }
+}
+*/
+
+int dp[51] = {0};
+
+int fibodp(int n)
+{
+    if (n == 1 || n == 2)
+    {
+        return dp[n] = 1;
+    }
+    if (dp[n] != 0)
+        return dp[n];
+    return dp[n] = fibodp(n - 1) + fibodp(n - 2);
+}
+
+int main()
+{
+    for (int i = 0; i < 30; i++)
     {
         printf("%d항 : %d\n", i, fibodp(i));
     }
