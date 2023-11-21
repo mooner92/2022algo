076224fd2d238d906ff1d43c66269bb5f2f895d0
Mooner92 <54096383+mooner92@users.noreply.github.com>
2023-11-21 18:23:39 +0900

    add

--- model_py_ex_1.py
+++ model_py_ex_1.py
@@ -1,10 +1,10 @@
 import torch
-from pathlib import Path
 from PIL import Image
+from pathlib import Path
 import os
 import sys
 
-if __name__ == "main":
+if __name__ == "__main__":
     # YOLOv5 모델을 로드합니다.
     model = torch.hub.load("ultralytics/yolov5:master", "yolov5s", pretrained=False)
     # 학습된 가중치 파일을 로드합니다.
@@ -12,22 +12,19 @@ if __name__ == "main":
     # 모델을 추론 모드로 설정합니다.
     model.eval()
 
-    # 이미지 디렉토리 경로
-    image_directory = sys.argv[1]
-    # 이미지 파일 리스트를 가져옵니다.
-    image_files = os.listdir(image_directory)
+    # 이미지 파일 경로
+    image_path = sys.argv[1]
 
-    # 결과 이미지를 저장할 디렉토리 경로
+    # 이미지를 모델에 전달하여 객체 감지 수행
+    results = model(image_path)
+
+    # 결과 이미지 저장할 디렉토리 경로
     output_directory = "/home/ubuntu/obok_backend_serv/build/libs/images/deep_images"
-    Path(output_directory).mkdir(parents=True, exist_ok=True)  # 디렉토리 생성
+    os.makedirs(output_directory, exist_ok=True)  # 디렉토리 생성
 
-    # 각 이미지에 대한 객체 감지 수행 및 결과 저장
-    for image_file in image_files:
-        # 이미지 파일 경로
-        image_path = os.path.join(image_directory, image_file)
-        # 이미지를 모델에 전달하여 객체 감지 수행
-        results = model(image_path)
-        # 결과 이미지 저장
-        output_image_path = os.path.join(output_directory, image_file)
-        results.show(save_path=output_image_path)
-    print("{output_directory}")
+    # 결과 이미지 파일 경로
+    output_image_path = os.path.join(output_directory, "result_image.jpg")
+
+    # 결과 이미지 저장
+    results.show(save_path=output_image_path)
+    print(output_image_path)
