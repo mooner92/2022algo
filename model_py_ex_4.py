9f1d018ba7d4efc94f7027cd96de7732da8c8cc6
Mooner92 <54096383+mooner92@users.noreply.github.com>
2023-12-05 00:25:30 +0900

    add

---
+++ model_py_ex_4.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+import sys
+
+# 프로젝트 디렉토리 경로 설정
+project_dir = "/home/mooner92/obok/deep_images"
+od_address = sys.argv[1]
+subprocess.run(
+    [
+        "python3",
+        "/home/mooner92/obok/yolov5/detect.py",
+        "--weights",
+        "/home/mooner92/obok/pyfile/best.pt",
+        "--img",
+        "640",
+        "--conf",
+        "0.01",
+        "--source",
+        od_address,
+        "--save-conf",
+        "--line-thickness",
+        "4",
+        "--save-txt",
+        "--project",
+        project_dir,
+    ]
+)
+
+od = od_address[26:-4]
+print(f"/home/mooner92/obok/deep_images{od}")
