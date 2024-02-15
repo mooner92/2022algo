001b9f198406cf667042ea07aa36aebc75feaecb
Mooner92 <54096383+mooner92@users.noreply.github.com>
2024-02-15 13:40:21 +0900

    add

--- recursion/resursion.cpp
+++ recursion/resursion.cpp
@@ -5,7 +5,7 @@ void countNum(int num)
 {
   if (num == 1)
   {
-    cout << "Num : " << num << endl;  //재귀함수는 탈출조건이 있어야함
+    cout << "Num : " << num << endl; // 재귀함수는 탈출조건이 있어야함
     return;
   }
   else
@@ -15,12 +15,11 @@ void countNum(int num)
   }
 }
 
-
 int main()
 {
   countNum(5);
   return 0;
 }
 
-
-//출처: https://see-ro-e.tistory.com/21 [SeeRoE 프로그래밍 기록]
+////
+// 출처: https://see-ro-e.tistory.com/21 [SeeRoE 프로그래밍 기록]
