8999287dea53e7db0b7f22b8d401cf84f4e80cc6
Mooner92 <54096383+mooner92@users.noreply.github.com>
2024-02-27 13:31:08 +0900

    add

--- jiwoo_queue_1.cpp
+++ jiwoo_queue_1.cpp
@@ -1,30 +1,39 @@
 #include <stdio.h>
 
-int main() {
-    int n,m,cnt=0,cnt2=0;
+int main()
+{
+    int n, m, cnt = 0, cnt2 = 0;
     char a;
     int st[100];
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf(" %c",&a);
-        if(a=='i'){
-            scanf("%d",&m);
-            st[cnt]=m;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf(" %c", &a);
+        if (a == 'i')
+        {
+            scanf("%d", &m);
+            st[cnt] = m;
             cnt++;
-            }
-        else if(a=='o'){
-            if(cnt==0){
+        }
+        else if (a == 'o')
+        {
+            if (cnt == 0)
+            {
                 printf("empty\n");
             }
-            else{printf("%d\n",st[cnt2]);
-                st[cnt2]=0;
+            else
+            {
+                printf("%d\n", st[cnt2]);
+                st[cnt2] = 0;
                 ++cnt2;
                 --cnt;
             }
-            }
-        else if(a=='c'){
-            printf("%d\n",cnt);
+        }
+        else if (a == 'c')
+        {
+            printf("%d\n", cnt);
         }
     }
     return 0;
 }
+///
